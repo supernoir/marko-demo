0c32323fd422f6f6ed69131a95eed090961962ab
Conny <kawohl.bentodesign@gmail.com>
2016-08-05 13:58:25 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.get('/', require('./src/pages/home'));
+
+app.listen(8080, function(){
+   console.log('Listening on port 8080');
+});
---
+++ src/pages/home/index.js
@@ -0,0 +1,4 @@
+module.exports = function(req, res) {
+   res.write('Hvar er du?');
+   res.end();
+}
